3d2b30e1865c2d27f51875455a04dc363cb7b537
Matteo Fusillo <matt.fusillo@gmail.com>
2019-11-22 13:51:57 +0000

    done tasks 5-6

--- pet_shop.rb
+++ pet_shop.rb
@@ -9,3 +9,11 @@ end
 def add_or_remove_cash(name, amount)
   return name[:admin][:total_cash] += amount
 end
+
+def pets_sold(name)
+  return name[:admin][:pets_sold]
+end
+
+def increase_pets_sold(name, amount)
+  return name[:admin][:pets_sold] += amount
+end
