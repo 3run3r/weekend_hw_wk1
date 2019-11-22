0ba3f55108c35848d658736f88a0c7f93211dac7
Matteo Fusillo <matt.fusillo@gmail.com>
2019-11-22 13:46:44 +0000

    finished first 4 tests

---
+++ pet_shop.rb
@@ -0,0 +1,11 @@
+def pet_shop_name(name)
+  return name[:name]
+end
+
+def total_cash(name)
+  return name[:admin][:total_cash]
+end
+
+def add_or_remove_cash(name, amount)
+  return name[:admin][:total_cash] += amount
+end
